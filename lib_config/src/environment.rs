b153cb0d5ac2283ae7444e3ce3c2dc640bfa7ba9
Joan Miquel Espada <joanmi@espada.cat>
2023-07-18 09:14:54 +0200

    removed comments and bump version

--- lib_config/src/environment.rs
+++ lib_config/src/environment.rs
@@ -20,7 +20,6 @@ pub struct EnvironmentVariables {
     rust_log: Option<String>,
     aws_region: Option<String>,
     aws_profile: Option<String>,
-    //aws_regions: Option<String>,
     aws_endpoint: Option<String>,
 
     contract_id: Option<u16>,
@@ -55,7 +54,6 @@ impl EnvironmentVariables {
             hmac_secret: None,
             rust_log: None,
             aws_region: None,
-            //aws_regions: None,
             aws_profile: None, 
             aws_endpoint: None,
             contract_id: None,
@@ -110,18 +108,6 @@ impl EnvironmentVariables {
     pub fn aws_profile(&self) -> Option<String> {
         self.aws_profile.clone()
     }
-    
-    // pub fn set_aws_regions(&mut self, value: String ) {
-    //     self.aws_regions = Some(value);
-    // }
-    // pub fn aws_regions(&self) -> Vec<String> {
-    //     match self.aws_regions.clone() {
-    //         None => return Vec::new(),
-    //         Some(items) =>{
-    //             return items.split(',').map(|i| i.to_string() ).collect();
-    //         }
-    //     }
-    // }
 
     pub fn aws_endpoint(&self) -> Option<String> {
         self.aws_endpoint.clone()
