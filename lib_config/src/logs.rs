41e1a3e00ebcc2cc4fdccceec58ea62e8f40fa20
Joan Miquel Espada <joanmi@espada.cat>
2023-10-06 15:31:42 +0200

    logs and traces

--- lib_config/src/logs.rs
+++ lib_config/src/logs.rs
@@ -7,7 +7,12 @@ static LOGGER_INITIALIZED: Once = Once::new();
 pub fn setup_log() {
 
     LOGGER_INITIALIZED.call_once(|| {
-        env_logger::init();
+        env_logger::Builder::from_default_env()
+            .format_timestamp_secs()
+            //.filter(None, log::LevelFilter::Info)
+            .format_indent(Some(2))
+            .init();
+        
         log::info!("Logger initialized");
     });
 
