509c4b6b2d7f10be171dc9d5dddd5cee46a838ce
Joan Miquel Espada <joanmi@espada.cat>
2023-10-16 20:15:43 +0200

    adding new lib

---
+++ lib_hash_objs/src/hash.rs
@@ -0,0 +1,14 @@
+//Data structure must match with data coming from matchapi
+
+//use std::{fmt, str::FromStr};
+
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct HashResult {
+    pub asset_id: Uuid,
+    #[serde(rename = "type")]  
+    pub ttype: String
+}
+
