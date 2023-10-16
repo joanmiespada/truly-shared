a97567720d9db2a8a02359f1260954aee0296f9d
Joan Miquel Espada <joanmi@espada.cat>
2023-10-16 11:54:45 +0200

    removed useless tags

--- lib_config/src/environment.rs
+++ lib_config/src/environment.rs
@@ -4,9 +4,9 @@ use serde::Deserialize;
 use url::Url;
 
 pub static ENV_VAR_ENVIRONMENT: &str = "ENVIRONMENT";
-pub static ENV_VAR_PROJECT_LABEL: &str = "PROJECT";
-pub static ENV_VAR_SERVICE_LABEL: &str = "SERVICE";
-pub static ENV_VAR_PROJECT: &str = "truly";
+//pub static ENV_VAR_PROJECT_LABEL: &str = "PROJECT";
+//pub static ENV_VAR_SERVICE_LABEL: &str = "SERVICE";
+//pub static ENV_VAR_PROJECT: &str = "truly";
 pub static DEV_ENV: &str = "development";
 pub static PROD_ENV: &str = "production";
 pub static STAGE_ENV: &str = "stage";
