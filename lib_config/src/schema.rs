5c8276007f0876d7af89657af19eb82dca330a58
Joan Miquel Espada <joanmi@espada.cat>
2023-10-16 19:18:30 +0200

    wait method

--- lib_config/src/schema.rs
+++ lib_config/src/schema.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use aws_sdk_dynamodb::types::TableStatus;
 use crate::config::Config;
 use crate::result::ResultE;
 
@@ -17,4 +18,30 @@ pub async fn schema_exists(config: &Config, table: &str) -> ResultE<bool> {
         Ok(list) => Ok(list.table_names().unwrap().contains(&table.into())),
         Err(e) => Err(e.into()),
     }
+}
+
+pub async fn wait_until_schema_is_active(
+    config: &Config,
+    table_name: &str,
+) -> ResultE<()> {
+    let client = aws_sdk_dynamodb::Client::new(config.aws_config());
+    loop {
+        let resp = client
+            .describe_table()
+            .table_name(table_name)
+            .send()
+            .await?;
+
+        match resp.table {
+            Some(table) => match table.table_status {
+                Some(status) if status == TableStatus::Active => break,
+                _ => (),
+            },
+            None => (),
+        };
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+    }
+
+    Ok(())
 }
