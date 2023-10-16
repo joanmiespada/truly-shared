1d84d266c99c8ef3f91ff8b76d94e49739cb8b05
Joan Miquel Espada <joanmi@espada.cat>
2023-10-16 18:41:06 +0200

    added schema exist

--- lib_config/src/schema.rs
+++ lib_config/src/schema.rs
@@ -7,3 +7,14 @@ pub trait Schema {
     async fn create_schema(config: &Config) -> ResultE<()>;
     async fn delete_schema(config: &Config) -> ResultE<()>;
 }
+
+pub async fn schema_exists(config: &Config, table: &str) -> ResultE<bool> {
+
+    let client = aws_sdk_dynamodb::Client::new(config.aws_config());
+    let table_list = client.list_tables().send().await;
+
+    match table_list {
+        Ok(list) => Ok(list.table_names().unwrap().contains(&table.into())),
+        Err(e) => Err(e.into()),
+    }
+}
