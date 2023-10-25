56203fcce925c8d13ea8518252d82cc81b6ed1d2
Joan Miquel Espada <joanmi@espada.cat>
2023-10-25 13:10:01 +0200

    missing pub

--- lib_hash_objs/src/similar_alert.rs
+++ lib_hash_objs/src/similar_alert.rs
@@ -4,15 +4,15 @@ use uuid::Uuid;
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct AlertExternalPayload {
     #[serde(rename = "type")]  
-    source_type: Option<String>,
-    origin_hash_id: Option<Uuid>,
-    origin_hash_type: Option<String>,
-    origin_frame_id: Option<Uuid>,
-    origin_frame_second: Option<f64>,
-    origin_frame_url: Option<String>,
-    origin_asset_id: Option<Uuid>,
-    similar_frame_id: Option<Uuid>,
-    similar_frame_second: Option<f64>,
-    similar_frame_url: Option<String>,
-    similar_asset_id: Option<Uuid>,
+    pub source_type: Option<String>,
+    pub origin_hash_id: Option<Uuid>,
+    pub origin_hash_type: Option<String>,
+    pub origin_frame_id: Option<Uuid>,
+    pub origin_frame_second: Option<f64>,
+    pub origin_frame_url: Option<String>,
+    pub origin_asset_id: Option<Uuid>,
+    pub similar_frame_id: Option<Uuid>,
+    pub similar_frame_second: Option<f64>,
+    pub similar_frame_url: Option<String>,
+    pub similar_asset_id: Option<Uuid>,
 }
